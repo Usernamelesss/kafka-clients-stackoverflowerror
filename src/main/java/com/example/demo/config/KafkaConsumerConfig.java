0a471bd50f9207a80beb3ba45b9d6ecd73080b94
Adriano <adriano.gigliuto@moveax.it>
2022-01-18 15:21:39 +0100

    Setup Kafka Consumer

---
+++ src/main/java/com/example/demo/config/KafkaConsumerConfig.java
@@ -0,0 +1,100 @@
+package com.example.demo.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.listener.adapter.RetryingMessageListenerAdapter;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "consumer.topic-1");
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+
+    @Bean
+    public ConcurrentMessageListenerContainer<String, String> kafkaContainer() {
+        ContainerProperties containerProperties = new ContainerProperties("consumer.topic-1");
+        containerProperties.setAckMode(ContainerProperties.AckMode.RECORD);
+        containerProperties.setAckOnError(false);
+        containerProperties.setSyncCommits(false);
+        containerProperties.setLogContainerConfig(true);
+
+        RetryingMessageListenerAdapter<String, String> adapter = new RetryingMessageListenerAdapter<>(messageHandler(), getRetryTemplate());
+        containerProperties.setMessageListener(adapter);
+
+        ConcurrentMessageListenerContainer<String, String> container = new ConcurrentMessageListenerContainer<>(
+                consumerFactory(), containerProperties);
+        container.setConcurrency(1);
+        container.setErrorHandler(new SeekToCurrentErrorHandler(
+                (record, exception) -> {
+                    adapter.onMessage((ConsumerRecord<String, String>) record);
+                }, new FixedBackOff(2000, FixedBackOff.UNLIMITED_ATTEMPTS)));
+        return container;
+    }
+
+    private MessageListener<String, String> messageHandler() {
+        return data -> System.out.println("Received " + data.value());
+    }
+
+    private RetryTemplate getRetryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setMaxInterval(10);
+        backOffPolicy.setMultiplier(2);
+
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(Integer.MAX_VALUE);
+
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
+}
